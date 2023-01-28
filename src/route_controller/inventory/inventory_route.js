cc8f024f08773059b0bb34f5a1cc5d525c8b25d6
krishnoit <krishnoit100@gmail.com>
2023-01-28 14:35:14 +0530

    :sparkles: feat : add the image upload route in inventory

--- src/route_controller/inventory/inventory_route.js
+++ src/route_controller/inventory/inventory_route.js
@@ -3,7 +3,9 @@ const inventory_route = express.Router();
 const auth = require("../../middlewares/auth");
 const {item_create} = require("./controller/inventory_post_ceontroller")
 const {item_get} = require("./controller/inventory_get_controller")
-
+const {fileUploadMiddleware} = require("../../middlewares/upload")
+const  {uploadfile} = require("./controller/upload_controller")
 inventory_route.post('/inventory',auth ,item_create)
 inventory_route.get('/inventory',auth ,item_get)
+inventory_route.post('/inventory/upload',auth ,fileUploadMiddleware,uploadfile)
 module.exports  = inventory_route
