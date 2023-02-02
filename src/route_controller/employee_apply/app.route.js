9f3021c3647b51839c507e4e129bc9e7ab98e41a
krishnoit <krishnoit100@gmail.com>
2023-02-02 05:48:09 +0530

    :sparkles: new api route set apply route

--- src/route_controller/employee_apply/app.route.js
+++ src/route_controller/employee_apply/app.route.js
@@ -5,11 +5,11 @@ const {apply_job_controller}= require("./controller/apply_controller")
 const {free_question}= require("./controller/apply_free_text.controller")
 const {single_choice}= require("./controller/apply_single_choice.controller")
 const {multi_choice}= require("./controller/apply_multi_choice.controller")
-
+const {getAllApply}= require("./controller/apply.get.all_controller")
 apply.post('/apply/job/:jobid',apply_job_controller);
 apply.post('/apply/job/free/:freeid',free_question);
 apply.post('/apply/job/single/:singleid',single_choice);
 apply.post('/apply/job/multi/:multiid',multi_choice);
-
+apply.get('/apply/:jobid',getAllApply)
 
 module.exports = apply
---
+++ src/route_controller/employee_apply/controller/apply.get.all_controller.js
@@ -0,0 +1,19 @@
+const Prisma = require("../../../../config/helper");
+async function getAllApply(req,res){
+    const id= req.params.id
+    try{
+        const data = await Prisma.apply.findMany({
+            where:{
+                job_id:id
+            }
+        })
+        res.status(200).json({
+            data:data
+        })
+    }catch(err){
+        res.status(500).json({
+            err:err
+        })
+    }
+}
+module.exports ={getAllApply}
