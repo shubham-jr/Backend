61fbfa34848c1b5b90cd04490f26e104258e61b2
krishnoit <krishnoit100@gmail.com>
2023-02-05 04:47:19 +0530

    more routes added to the modules

--- src/route_controller/contract/contract_router.js
+++ src/route_controller/contract/contract_router.js
@@ -3,10 +3,11 @@ const contract = express.Router();
 const{postPersonal} = require('./controller/postPersonal.js')
 const {postApra} =require('./controller/postApraFund')
 const {postSmsf} =require('./controller/postSmsf')
-
+const {superFund} =require('./controller/postSuperFund')
 
 contract.post('/contract/personal',postPersonal)
 contract.post('/contract/apra',postApra)
 contract.post('/contract/smsf',postSmsf)
+contract.post('/contract/smsf',superFund)
 
 module.exports =contract
