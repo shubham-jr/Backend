4c0fe1336b2ea004b1aa0594670b0db5d0dbf20f
krishnoit <krishnoit100@gmail.com>
2023-02-05 06:28:45 +0530

    upload route set for apra

--- src/route_controller/contract/contract_router.js
+++ src/route_controller/contract/contract_router.js
@@ -5,11 +5,20 @@ const {postApra} =require('./controller/postApraFund')
 const {postSmsf} =require('./controller/postSmsf')
 const {postSuperFund} =require('./controller/postSuperFund')
 const {postBakeryInduction} =require('./controller/postBakeryInduction')
-
+const {fileUploadMiddleware} = require("../../middlewares/upload")
 contract.post('/contract/personal',postPersonal)
 contract.post('/contract/apra',postApra)
 contract.post('/contract/smsf',postSmsf)
 contract.post('/contract/superfund',postSuperFund)
 contract.post('/contract/BakeryIndcution',postBakeryInduction)
+
+
+
+/// file upload route set
+
+contract.post(
+    "/contract/upload/apra",
+    fileUploadMiddleware 
+  );
 
 module.exports =contract
